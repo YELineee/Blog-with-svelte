01fa8f4182c2aa8c0633c672e9509ffffbe93682
YEL!ne <1638317920@qq.com>
2023-03-16 01:24:30 +0800

    Create loadGoogleFont.js

---
+++ src/JsPlugin/loadGoogleFont.js
@@ -0,0 +1,11 @@
+// global.js
+
+export function loadGoogleFont() {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://fonts.googleapis.com/css2?family=Courgette&display=swap';
+    document.head.appendChild(link);
+  }
+  
+
+  
