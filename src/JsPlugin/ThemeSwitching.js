2726b519073a3ad1869aaa4b6c6b549c822d16c7
YEL!ne <1638317920@qq.com>
2023-03-16 02:05:53 +0800

    update

---
+++ src/JsPlugin/ThemeSwitching.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const ithem = writable(false); // 初始值为false
--- src/routes/TEST/+page.svelte
+++ src/routes/TEST/+page.svelte
@@ -2,10 +2,12 @@
   import Typewriter from 'svelte-typewriter'
   import { onMount } from 'svelte';
   import { loadGoogleFont} from '../../JsPlugin/loadGoogleFont';
+  import { ithem } from '../../JsPlugin/ThemeSwitching'
 
 
   onMount(() => {
     loadGoogleFont();
+    toggleMode();
   });
 
 
@@ -13,8 +15,23 @@
 
   // Iterm
 
-  let mode ="&quot; turn on the light &quot;"
+  let isOn = false;
+  let mode = '';
+ 
+  function toggleDark() {
+    ithem.update(d => !d);
+  }
 
+  function toggleMode() {
+    isOn = !isOn;
+    if (isOn) {
+      mode = '&quot; turn on the light &quot;';
+      toggleDark();
+    } else {
+      mode = '&quot; turn off the light &quot;';
+      toggleDark();
+    }
+  }
   //Iterm
 
  
@@ -22,29 +39,31 @@
 
 
 <!--                               -----TEXBAR-----                                         -->
+<div data-theme="{$ithem ? "dark" : "light"}">
 
-<div class="container w-10/12 h-4/6 mx-auto  pt-6  overflow-hidden ">
-    
-  <div >
-    <Typewriter>
-      <p  class="font-dynamic-p-titl  font-black  "> <a href="./ME" class="link link-hover line-through text-yellow-400">YEL!ne</a></p>  
-      <p  class="font-dynamic-p-text  font-medium ml-8 mt-8 overflow-clip ">&nbsp; &nbsp; Everyone thought he was a bit <a href="https://github.com/YELineee/OUT_OT_LUNCH" class="link link-hover text-blue-400"> " Out To Lunch "</a> . He  has left some <a href="./NOTE" class="link link-hover text-indigo-400">" Notes "</a> , it seems he doesn't have a very good memory, BTW , his name is <a href="./ME" class="link link-hover text-green-400"> YELine</a>.</p>
-      <p  class="font-dynamic-p-text  font-medium ml-8 mt-4 overflow-clip"> &nbsp; &nbsp; Do you need me to 
-          <a href="./404" class="link link-hover  text-red-400">
-            {mode}
-          </a> 
-        
-        for you? Or Switch to <a href="./zh-CN" class="link link-hover text-amber-400"> Chinese </a>?
-      </p>
 
-    
-    <p   class="font-dynamic-p-text "></p>
+  <div class="container w-10/12 h-4/6 mx-auto  pt-6  overflow-hidden ">
       
-    </Typewriter>
+    <div >
+      <Typewriter>
+        <p  class="font-dynamic-p-titl  font-black  "> <a href="./ME" class="link link-hover line-through text-yellow-400">YEL!ne</a></p>  
+        <p  class="font-dynamic-p-text  font-medium ml-8 mt-8 overflow-clip ">&nbsp; &nbsp; Everyone thought he was a bit <a href="https://github.com/YELineee/OUT_OT_LUNCH" class="link link-hover text-blue-400"> " Out To Lunch "</a> . He  has left some <a href="./NOTE" class="link link-hover text-indigo-400">" Notes "</a> , it seems he doesn't have a very good memory, BTW , his name is <a href="./ME" class="link link-hover text-green-400"> YELine</a>.</p>
+        <p  class="font-dynamic-p-text  font-medium ml-8 mt-4 overflow-clip"> &nbsp; &nbsp; Do you need me to 
+            <a class="link link-hover  text-red-400" on:click={toggleMode}>
+              {mode}
+            </a> 
+          
+          for you? Or Switch to <a href="./zh-CN" class="link link-hover text-amber-400"> Chinese </a>?
+        </p>
+
+      
+      <p   class="font-dynamic-p-text "></p>
+        
+      </Typewriter>
+    </div>
+
   </div>
-
 </div>
-
 <!--                               -----TEXBAR-----                                         -->
   
 
