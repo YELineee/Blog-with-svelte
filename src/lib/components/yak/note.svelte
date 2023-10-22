9d4c833c89621a10f6864eeec18896b2ae3921b3
YEL!ne <1638317920@qq.com>
2023-10-22 20:34:01 +0800

    Create note.svelte

---
+++ src/lib/components/yak/note.svelte
@@ -0,0 +1,58 @@
+<div class="">
+    
+	<div class="md:leading-loose font-thin text-xl md:text-5xl text-black">
+		<p>
+			<span
+				>&nbsp; &nbsp;You might have seen our story on
+				<a href="" class="underline decoration-double decoration-2" rel="external"> <span>roasting your Spotify</span></a>
+				<img class="w-6 md:w-16" src="./images/IMG_4999.webp" alt="story thumbnail" />, but we’ve also made
+				stuff about
+				<a href="" class="underline decoration-double decoration-2" rel="external"> <span>women’s pockets</span></a>
+				<img class="w-6 md:w-16" src="./images/IMG_5001.webp" alt="story thumbnail" /> and
+				<a href="" rel="external"> <span>rapper vocabularies</span></a>
+				<img
+					class="w-6 md:w-16"
+					src="./images/IMG_4999.webp"
+					alt="story thumbnail"
+				/>
+			</span>
+
+			<span class=""
+				>You might have seen our story on
+				<a href="" class="underline decoration-double decoration-2" rel="external"> <span>roasting your Spotify</span></a>
+				<img class="w-6 md:w-16" src="./images/IMG_4999.webp" alt="story thumbnail" />, but we’ve also made
+				stuff about
+				<a href="" rel="external"> <span>women’s pockets</span></a>
+				<img src="common/assets/thumbnails/320/2018_08_pockets.jpg" alt="story thumbnail" /> and
+				<a href="" rel="external"> <span>rapper vocabularies</span></a>
+				<img
+					class="w-6 md:w-16"
+					src="./images/IMG_4999.webp"
+					alt="story thumbnail"
+				/>.asdadsdasdsaadsasd</span
+			>
+		</p>
+	</div>
+</div>
+
+<style>
+	img {
+		display: inline;
+		word-wrap: break-word;
+	}
+    a {
+        transition: background-color 1s cubic-bezier(.0,.0,.50,.50);
+    }
+
+    a:hover {
+        background-color: blue;
+    }
+
+	a:hover + img {
+        transform: scale(1.2);
+    }
+	
+    /* p{
+        font-size: 3vw;
+    } */
+</style>
